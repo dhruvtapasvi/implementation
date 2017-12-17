d71202df31a9046b3f048bc396b83bd034476d0b
Dhruv Tapasvi <dhruv.tapasvi1996@gmail.com>
2017-12-17 18:01:17 +0000

    Added sscripts for obtaining norb datasets

---
+++ scripts/retrieveNORB.sh
@@ -0,0 +1,19 @@
+cd res
+mkdir norb
+cd norb
+
+curl https://cs.nyu.edu/~ylclab/data/norb-v1.0-small/smallnorb-5x46789x9x18x6x2x96x96-training-dat.mat.gz -o norb_train_image.gz
+curl https://cs.nyu.edu/~ylclab/data/norb-v1.0-small/smallnorb-5x46789x9x18x6x2x96x96-training-cat.mat.gz -o norb_train_category.gz
+curl https://cs.nyu.edu/~ylclab/data/norb-v1.0-small/smallnorb-5x46789x9x18x6x2x96x96-training-info.mat.gz -o norb_train_info.gz
+curl https://cs.nyu.edu/~ylclab/data/norb-v1.0-small/smallnorb-5x01235x9x18x6x2x96x96-testing-dat.mat.gz -o norb_test_image.gz
+curl https://cs.nyu.edu/~ylclab/data/norb-v1.0-small/smallnorb-5x01235x9x18x6x2x96x96-testing-cat.mat.gz -o norb_test_category.gz
+curl https://cs.nyu.edu/~ylclab/data/norb-v1.0-small/smallnorb-5x01235x9x18x6x2x96x96-testing-info.mat.gz -o norb_test_info.gz
+
+gunzip norb_train_image.gz
+gunzip norb_train_category.gz
+gunzip norb_train_info.gz
+gunzip norb_test_image.gz
+gunzip norb_test_category.gz
+gunzip norb_test_info.gz
+
+cd ../..
