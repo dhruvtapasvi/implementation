c6122d0bcbd581a52d2e423d0dea755c597ab9a8
Dhruv Tapasvi <dhruv.tapasvi1996@gmail.com>
2017-12-20 14:44:28 +0000

    Added TODO to test std normalisation later

--- src/preprocess/Normalise.py
+++ src/preprocess/Normalise.py
@@ -6,4 +6,5 @@ class Normalise(Preprocessor):
     def preprocess(self, trainData: np.ndarray, testData: np.ndarray) -> (np.ndarray, np.ndarray):
         trainDataStd = np.std(trainData, 0)
         # TODO: What about divide by zero?
+        # TODO: Write test
         return trainData / trainDataStd, testData / trainDataStd
