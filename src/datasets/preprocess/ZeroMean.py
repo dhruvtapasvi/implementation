04131a03b0bafe448065001032be0692fc3d88cd
Dhruv Tapasvi <dhruv.tapasvi1996@gmail.com>
2018-02-25 18:00:30 +0000

    Deleted former preprocessing module

--- src/datasets/preprocess/ZeroMean.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-from datasets.preprocess import Preprocessor
-
-
-class ZeroMean(Preprocessor):
-    def preprocess(self, trainData: np.ndarray, testData: np.ndarray) -> (np.ndarray, np.ndarray):
-        trainDataMean = np.mean(trainData, 0)
-        return trainData - trainDataMean, testData - trainDataMean
