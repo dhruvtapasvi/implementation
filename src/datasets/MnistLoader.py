80b9cd43d9263ec22e83c10d39b5be84391cc64b
Dhruv Tapasvi <dhruv.tapasvi1996@gmail.com>
2018-02-25 15:06:20 +0000

    Added third validation split return value to loadData function of DatasetLoader; MnistLoader now implements this improved interface

--- src/datasets/MnistLoader.py
+++ src/datasets/MnistLoader.py
@@ -2,6 +2,13 @@ from datasets.DatasetLoader import DatasetLoader
 import numpy as np
 from keras.datasets import mnist
 
+
 class MnistLoader(DatasetLoader):
-    def loadData(self) -> ((np.ndarray, np.ndarray), (np.ndarray, np.ndarray)):
-        return mnist.load_data()
+    __MNIST_VALIDATION_SPLIT = 50000
+
+    def loadData(self) -> ((np.ndarray, np.ndarray), (np.ndarray, np.ndarray), (np.ndarray, np.ndarray)):
+        (XTest, YTest), (XTrain, YTrain) = mnist.load_data()
+        return\
+            (XTrain[:self.__MNIST_VALIDATION_SPLIT], YTrain[:self.__MNIST_VALIDATION_SPLIT]),\
+            (XTrain[self.__MNIST_VALIDATION_SPLIT:], YTrain[self.__MNIST_VALIDATION_SPLIT:]),\
+            (XTest, YTest)
