95974cfb5b5d856a07cae782d1de9c2a0a0d2b62
Dhruv Tapasvi <dhruv.tapasvi1996@gmail.com>
2018-03-09 10:12:55 +0000

    Added a dense schema and instance and validation

--- testScripts/validateSchemas.sh
+++ testScripts/validateSchemas.sh
@@ -6,3 +6,6 @@ jsonschema -i config/schema/convolutional.schema.json config/schema/json-schema-
 
 # Test convolutional vae model configuration instances against schema
 jsonschema -i config/model/convolutional/mnist_conv_bce_2.json config/schema/convolutional.schema.json
+
+# Test dense vae model configuration instances against schema
+jsonschema -i config/model/dense/mnist_dense_keras_autoencoders_tutorial.json config/schema/dense.schema.json
