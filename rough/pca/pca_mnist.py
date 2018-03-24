e4dab963805ca0bbc5f4ab06f5af0c73a82c6c33
Dhruv Tapasvi <dhruv.tapasvi1996@gmail.com>
2018-03-24 10:48:01 +0000

    Wrote pca example

---
+++ rough/pca/pca_mnist.py
@@ -0,0 +1,19 @@
+from dataset.basicLoader.MnistLoader import MnistLoader
+from dataset.preprocessLoader.ScaleBetweenZeroAndOne import ScaleBetweenZeroAndOne
+import numpy as np
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+
+
+mnistLoader = ScaleBetweenZeroAndOne(MnistLoader(), 0, 255)
+(xTrain, yTrain), (xVal, yVal), (xTest, yTest) = mnistLoader.loadData()
+xTrain = xTrain.reshape(xTrain.shape[0],xTrain.shape[1]*xTrain.shape[2])
+print(xTrain.shape)
+
+pca = PCA(n_components=100)
+pca.fit(xTrain)
+
+plt.plot(np.cumsum(pca.explained_variance_ratio_))
+plt.xlabel('Number of components')
+plt.ylabel('Cumulative explained variance)')
+plt.show()
--- scripts/copyToCozy.sh
+++ scripts/copyToCozy.sh
@@ -3,4 +3,4 @@
 scp -o "GSSAPIAuthentication=yes" -r src ddt21@cozy.cl.cam.ac.uk:~/implementation
 #scp -o "GSSAPIAuthentication=yes" -r rough ddt21@cozy.cl.cam.ac.uk:~/implementation
 #scp -o "GSSAPIAuthentication=yes" -r scripts ddt21@cozy.cl.cam.ac.uk:~/implementation
-#scp -o "GSSAPIAuthentication=yes" -r config ddt21@cozy.cl.cam.ac.uk:~/implementation
+scp -o "GSSAPIAuthentication=yes" -r config ddt21@cozy.cl.cam.ac.uk:~/implementation
