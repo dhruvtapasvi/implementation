c3d0af616c70dc4b24971117d5bdaa2c4ac4ec10
Dhruv Tapasvi <ddt21@cl.cam.ac.uk>
2018-03-24 12:04:06 +0000

    Perform 100 component pca for mnist and norb, and save result

--- src/rough/pca/pca_mnist.py
+++ src/rough/pca/pca_mnist.py
@@ -1,6 +1,8 @@
 import matplotlib
 matplotlib.use('Agg')
 
+import pickle
+import time
 from dataset.basicLoader.MnistLoader import MnistLoader
 from dataset.preprocessLoader.ScaleBetweenZeroAndOne import ScaleBetweenZeroAndOne
 import numpy as np
@@ -13,10 +15,20 @@ mnistLoader = ScaleBetweenZeroAndOne(MnistLoader(), 0, 255)
 xTrain = xTrain.reshape(xTrain.shape[0],xTrain.shape[1]*xTrain.shape[2])
 print(xTrain.shape)
 
-pca = PCA(n_components=500)
+pca = PCA(n_components=100)
+
+start = time.time()
 pca.fit(xTrain)
+end = time.time()
+elapsed = end-start
+print("Time taken: {}".format(elapsed))
+
+for i in range(100):
+    print(i, pca.explained_variance_ratio_[i])
 
 plt.plot(np.cumsum(pca.explained_variance_ratio_))
 plt.xlabel('Number of components')
 plt.ylabel('Cumulative explained variance)')
-plt.savefig('mnist_pca_graph')
+plt.savefig('mnist_pca_graph.png')
+
+pickle.dump(pca, open("./mnist_pca_100.p", "wb"))
--- src/rough/pca/pca_norb.py
+++ src/rough/pca/pca_norb.py
@@ -1,6 +1,7 @@
 import matplotlib
 matplotlib.use('Agg')
 
+import pickle
 from dataset.basicLoader.NorbLoader import NorbLoader
 from dataset.preprocessLoader.ScaleBetweenZeroAndOne import ScaleBetweenZeroAndOne
 import numpy as np
@@ -14,7 +15,7 @@ norbLoader = ScaleBetweenZeroAndOne(NorbLoader("../res/norb"), 0, 255)
 xTrain = xTrain.reshape(xTrain.shape[0],xTrain.shape[1]*xTrain.shape[2])
 print(xTrain.shape)
 
-pca = PCA(n_components=1000)
+pca = PCA(n_components=100)
 
 start = time.time()
 pca.fit(xTrain)
@@ -22,8 +23,13 @@ end = time.time()
 elapsed = end - start
 print("Time elapsed: {}".format(elapsed))
 
+for i in range(100):
+    print(i, pca.explained_variance_ratio_[i])
+
 plt.plot(np.cumsum(pca.explained_variance_ratio_))
 plt.xlabel('Number of components')
 plt.ylim(0, 1.0)
 plt.ylabel('Cumulative explained variance)')
 plt.savefig('norb_pca_graph')
+
+pickle.dump(pca, open("./norb_pca_100.p", "wb"))
