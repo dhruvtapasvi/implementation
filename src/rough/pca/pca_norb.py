2c0c0b82075c7e5199cdd8e32a82d8b7219172f8
Dhruv Tapasvi <dhruv.tapasvi1996@gmail.com>
2018-03-24 11:29:08 +0000

    Refine rough pca norb

--- src/rough/pca/pca_norb.py
+++ src/rough/pca/pca_norb.py
@@ -6,6 +6,7 @@ from dataset.preprocessLoader.ScaleBetweenZeroAndOne import ScaleBetweenZeroAndO
 import numpy as np
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
+import time
 
 
 norbLoader = ScaleBetweenZeroAndOne(NorbLoader("../res/norb"), 0, 255)
@@ -13,10 +14,16 @@ norbLoader = ScaleBetweenZeroAndOne(NorbLoader("../res/norb"), 0, 255)
 xTrain = xTrain.reshape(xTrain.shape[0],xTrain.shape[1]*xTrain.shape[2])
 print(xTrain.shape)
 
-pca = PCA(n_components=xTrain.shape[1])
+pca = PCA(n_components=1000)
+
+start = time.time()
 pca.fit(xTrain)
+end = time.time()
+elapsed = end - start
+print("Time elapsed: {}".format(elapsed))
 
 plt.plot(np.cumsum(pca.explained_variance_ratio_))
 plt.xlabel('Number of components')
+plt.ylim(0, 1.0)
 plt.ylabel('Cumulative explained variance)')
 plt.savefig('norb_pca_graph')
