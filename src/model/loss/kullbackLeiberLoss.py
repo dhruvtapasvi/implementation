d8a707511bde1705884e9bf5a3d5e2217811fab8
Dhruv Tapasvi <dhruv.tapasvi1996@gmail.com>
2018-03-27 13:42:56 +0100

    Refactored KL Loss

--- src/model/loss/kullbackLeiberLoss.py
+++ src/model/loss/kullbackLeiberLoss.py
@@ -3,6 +3,7 @@ from keras.backend import sum, square, mean, exp
 
 def kullbackLeiberLossConstructor(latentRepresentationMean, latentLogVariance):
     def kullbackLeiberLoss(inputRepresentation, decodedInputRepresentation):
-        return mean(- 0.5 * sum(1 + latentLogVariance - square(latentRepresentationMean) - exp(latentLogVariance), axis=-1))
+        lossPerImage = - 0.5 * sum(1 + latentLogVariance - square(latentRepresentationMean) - exp(latentLogVariance), axis=-1)
+        return mean(lossPerImage)
 
     return kullbackLeiberLoss
