0cc67f34de504eea616105241d1ef8ac86953e4e
Dhruv Tapasvi <dhruv.tapasvi1996@gmail.com>
2018-03-27 15:02:10 +0100

    Added factor of 2 to pi

--- src/model/loss/meanSquaredErrorLoss.py
+++ src/model/loss/meanSquaredErrorLoss.py
@@ -10,14 +10,14 @@ def meanSquaredErrorLossConstructor(inputRepresentationDimensions, decodedInputR
 
     def meanSquaredErrorLoss(inputRepresentation, decodedInputRepresentation):
         if decodedInputRepresentationVariance is not None:
-            lossPerPixel = 0.5 * math.log(math.pi) + 0.5 * log(decodedInputRepresentationVariance + epsilon) + 0.5 * square(decodedInputRepresentation - inputRepresentation) / (decodedInputRepresentationVariance + epsilon)
+            lossPerPixel = 0.5 * math.log(2 * math.pi) + 0.5 * log(decodedInputRepresentationVariance + epsilon) + 0.5 * square(decodedInputRepresentation - inputRepresentation) / (decodedInputRepresentationVariance + epsilon)
             # lossPerPixel = square(decodedInputRepresentation - inputRepresentation)
             imageLoss = sum(lossPerPixel, axis=-1)
             return mean(imageLoss)
         else:
             # Assume variance is 1
             return mean(totalNumberOfPixels * 0.5 * (
-                math.log(math.pi) + mean_squared_error(flatten(inputRepresentation), flatten(decodedInputRepresentation))
+                math.log(2 * math.pi) + mean_squared_error(flatten(inputRepresentation), flatten(decodedInputRepresentation))
             ))
 
     return meanSquaredErrorLoss
