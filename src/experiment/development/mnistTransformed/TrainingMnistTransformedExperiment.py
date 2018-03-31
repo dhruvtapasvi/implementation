6c24c10605682b482ee46a9563265fc4c003736e
Dhruv Tapasvi <dhruv.tapasvi1996@gmail.com>
2018-03-31 16:37:53 +0100

    Train mnist experiment

--- src/experiment/development/mnistTransformed/TrainingMnistTransformedExperiment.py
+++ src/experiment/development/mnistTransformed/TrainingMnistTransformedExperiment.py
@@ -1,9 +1,27 @@
 from dataset.basicLoader.MnistTransformedLoader import MnistTransformedLoader
+from dataset.preprocessLoader.ScaleBetweenZeroAndOne import ScaleBetweenZeroAndOne
 from experiment.Experiment import Experiment
+from config.ConvolutionAutoencoderConfig import ConvolutionalAutoencoderConfig
+import pickle
 
 
 class TrainingMnistTransformedExperiment(Experiment):
     def run(self):
-        mnistLoader = MnistTransformedLoader("./res/mnistTransformed_2")
-        (_, yTrain), _, _ = mnistLoader.loadData()
-        print(yTrain[0:10])
+        rootPath = ".."
+
+        config = ConvolutionalAutoencoderConfig(rootPath + "/config/model/convolutional/mnist_transformed_conv_7_8_256_10_bce.json")
+        vae = config.fromConfig()
+        vae.buildModels()
+        vae.summary()
+
+        mnistLoader = ScaleBetweenZeroAndOne(MnistTransformedLoader(rootPath + "/res/mnistTransformed_1"), 0, 255)
+        (xTrain, yTrain), (xVal, yVal), _ = mnistLoader.loadData()
+
+        # Train model
+        batchSize = 1000
+        epochs = 50
+        trainingHistory = vae.train(xTrain, xVal, epochs, batchSize)
+
+        # Save training history and network weights:
+        pickle.dump(trainingHistory.history, open(rootPath + "/modelTrainingHistory/" + config.stringDescriptor + ".history.p", "wb"))
+        vae.saveWeights(rootPath + "/cacheWeights/" + config.stringDescriptor + ".weights.h5")
