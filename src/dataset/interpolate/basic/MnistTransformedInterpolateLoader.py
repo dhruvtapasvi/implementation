f9397c94082781c7e1e7e1303e2e9f46eeb974eb
Dhruv Tapasvi <dhruv.tapasvi1996@gmail.com>
2018-04-02 19:37:31 +0100

    Added simplifying interfaces for MnistInterpolateLoader and MnistTransformedInterpolateLoader

--- src/dataset/interpolate/basic/MnistTransformedInterpolateLoader.py
+++ src/dataset/interpolate/basic/MnistTransformedInterpolateLoader.py
@@ -1,17 +1,22 @@
 import numpy as np
 
+from dataset.loader.DatasetLoader import DatasetLoader
 from dataset.interpolate.InterpolateDatasetLoader import InterpolateDatasetLoader
+from dataset.interpolate.process.CreateTransformedInterpolateData import CreateTransformedInterpolateData
 import dataset.info.MnistTransformedInfo as mnistTransformedInfo
 
 
 class MnistTransformedInterpolateLoader(InterpolateDatasetLoader):
-    def __init__(self, mnistTransformedInterpolateHome: str):
-        self.__mnistTransformedInterpolateHome = mnistTransformedInterpolateHome
+    def __init__(self, mnistLoader: DatasetLoader):
+        self.__mnistLoader = CreateTransformedInterpolateData(
+            mnistLoader,
+            mnistTransformedInfo.PADDING,
+            *mnistTransformedInfo.DEFAULT_JOINT_FACTORS,
+            *mnistTransformedInfo.INTERPOLATE_JOINT_FACTORS
+        )
 
     def loadInterpolationData(self):
-        xTestInterpolate = np.load(self.__mnistTransformedInterpolateHome + "/xInterpolate.npy")
-        yTestInterpolate = np.load(self.__mnistTransformedInterpolateHome + "/yInterpolate.npy")
-        return xTestInterpolate, yTestInterpolate
+        return self.__mnistLoader.loadInterpolationData()
 
     def dataPointShape(self):
         return mnistTransformedInfo.IMAGE_DIMENSIONS, mnistTransformedInfo.LABEL_DIMENSIONS
