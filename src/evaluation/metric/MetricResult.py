365f3a39e1f6b18701cbd571cd65efa9bf735758
Dhruv Tapasvi <dhruv.tapasvi1996@gmail.com>
2018-04-22 18:37:11 +0100

    Add result MetricResult class

---
+++ src/evaluation/metric/MetricResult.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+class MetricResult:
+    def __init__(self, allValues: np.ndarray):
+        self.__allValues = allValues
+        self.__mean = np.mean(allValues)
+        self.__standardDeviation = np.std(allValues)
+
+    @property
+    def allValues(self) -> np.ndarray:
+        return self.__allValues
+
+    @property
+    def mean(self) -> float:
+        return self.__mean
+
+    @property
+    def standardDeviation(self) -> float:
+        return self.__standardDeviation
