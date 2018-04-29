26cc580656084c989c7db155bc58c44582bd602b
Dhruv Tapasvi <dhruv.tapasvi1996@gmail.com>
2018-04-29 16:54:33 +0100

    Compute binary crossentropy in batches

--- src/evaluation/metric/BinaryCrossEntropy.py
+++ src/evaluation/metric/BinaryCrossEntropy.py
@@ -4,9 +4,24 @@ import numpy as np
 from keras.backend import variable, binary_crossentropy, sum, eval
 
 
+BLOCK_SIZE = 1000
+
+
 class BinaryCrossEntropy(Metric):
     def compute(self, first: np.ndarray, second: np.ndarray) -> MetricResult:
-        firstFlattened = variable(first.reshape((first.shape[0], -1)))
-        secondFlattened = variable(second.reshape((second.shape[0], -1)))
-        result = eval(sum(binary_crossentropy(firstFlattened, secondFlattened), axis=-1))
+        numElements = len(first)
+        result = np.zeros((numElements,), dtype='float64')
+
+        for i in range(numElements/BLOCK_SIZE):
+            startIndex = BLOCK_SIZE * i
+            endIndex = min(BLOCK_SIZE * (i+1), numElements)
+
+            firstBlock = first[startIndex:endIndex]
+            secondBlock = second[startIndex:endIndex]
+
+            firstBlockFlattened = variable(firstBlock.reshape((len(firstBlock), -1)))
+            secondBlockFlattened = variable(secondBlock.reshape((len(secondBlock), -1)))
+
+            result[startIndex:endIndex] = eval(sum(binary_crossentropy(firstBlockFlattened, secondBlockFlattened), axis=-1))
+
         return MetricResult(result)
