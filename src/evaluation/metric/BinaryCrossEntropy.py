33c81b2d7825788573d6e4e2d38081adbc28977b
Dhruv Tapasvi <dhruv.tapasvi1996@gmail.com>
2018-04-29 18:40:58 +0100

    Use separate tensorflow session

--- src/evaluation/metric/BinaryCrossEntropy.py
+++ src/evaluation/metric/BinaryCrossEntropy.py
@@ -1,7 +1,8 @@
 from evaluation.metric.Metric import Metric
 from evaluation.metric.MetricResult import MetricResult
 import numpy as np
-from keras.backend import variable, binary_crossentropy, sum, eval
+from keras.backend import variable, binary_crossentropy, sum, eval, get_session, set_session
+import tensorflow as tf
 
 
 BLOCK_SIZE = 1000
@@ -9,19 +10,30 @@ BLOCK_SIZE = 1000
 
 class BinaryCrossEntropy(Metric):
     def compute(self, first: np.ndarray, second: np.ndarray) -> MetricResult:
-        numElements = len(first)
-        result = np.zeros((numElements,), dtype='float64')
+        oldSession = get_session()
 
-        for i in range(numElements // BLOCK_SIZE):
-            startIndex = BLOCK_SIZE * i
-            endIndex = min(BLOCK_SIZE * (i+1), numElements)
+        with tf.Session() as sess:
+            set_session(sess)
+            firstFlattened = variable(first.reshape((first.shape[0], -1)))
+            secondFlattened = variable(second.reshape((second.shape[0], -1)))
+            result = eval(sum(binary_crossentropy(firstFlattened, secondFlattened), axis=-1))
 
-            firstBlock = first[startIndex:endIndex]
-            secondBlock = second[startIndex:endIndex]
+        set_session(oldSession)
 
-            firstBlockFlattened = variable(firstBlock.reshape((len(firstBlock), -1)))
-            secondBlockFlattened = variable(secondBlock.reshape((len(secondBlock), -1)))
 
-            result[startIndex:endIndex] = eval(sum(binary_crossentropy(firstBlockFlattened, secondBlockFlattened), axis=-1))
+        # numElements = len(first)
+        # result = np.zeros((numElements,), dtype='float64')
+        #
+        # for i in range(numElements // BLOCK_SIZE):
+        #     startIndex = BLOCK_SIZE * i
+        #     endIndex = min(BLOCK_SIZE * (i+1), numElements)
+        #
+        #     firstBlock = first[startIndex:endIndex]
+        #     secondBlock = second[startIndex:endIndex]
+        #
+        #     firstBlockFlattened = variable(firstBlock.reshape((len(firstBlock), -1)))
+        #     secondBlockFlattened = variable(secondBlock.reshape((len(secondBlock), -1)))
+        #
+        #     result[startIndex:endIndex] = eval(sum(binary_crossentropy(firstBlockFlattened, secondBlockFlattened), axis=-1))
 
         return MetricResult(result)
--- src/experiment/experimentalConfigTuples.py
+++ src/experiment/experimentalConfigTuples.py
@@ -7,18 +7,18 @@ from evaluation.metric.BinaryCrossEntropy import BinaryCrossEntropy
 
 experimentalConfigTuples = [
     # MNIST
-    ExperimentalConfigTuple(datasetPackages.mnistPackage, configs.conv_28x28_3_8_ENC_1024_DEC_1024_LAT_32_bce, 500, 100, SquaredError(1), SquaredError(1.0)),
-    ExperimentalConfigTuple(datasetPackages.mnistPackage, configs.deepDense_28x28_ENC_512x2_1024_DEC_512x3_LAT_32_bce, 500, 200, SquaredError(1), SquaredError(1.0)),
+    ExperimentalConfigTuple(datasetPackages.mnistPackage, configs.conv_28x28_3_8_ENC_1024_DEC_1024_LAT_32_bce, 500, 100, SquaredError(1), BinaryCrossEntropy()),
+    ExperimentalConfigTuple(datasetPackages.mnistPackage, configs.deepDense_28x28_ENC_512x2_1024_DEC_512x3_LAT_32_bce, 500, 200, SquaredError(1), BinaryCrossEntropy()),
 
     # MNIST transformed
-    ExperimentalConfigTuple(datasetPackages.mnistTransformedLimitedRotationPackage, configs.deepDense_64x64_ENC_1024x4_DEC_1024x4_LAT_32_bce, 500, 100, SquaredError(1), SquaredError(1.0)),
-    ExperimentalConfigTuple(datasetPackages.mnistTransformedLimitedRotationPackage, configs.conv_64x64_6_16_ENC_1024x3_DEC_1024x3_LAT_32_bce, 500, 100, SquaredError(1), SquaredError(1.0)),
+    ExperimentalConfigTuple(datasetPackages.mnistTransformedLimitedRotationPackage, configs.deepDense_64x64_ENC_1024x4_DEC_1024x4_LAT_32_bce, 500, 100, SquaredError(1), BinaryCrossEntropy()),
+    ExperimentalConfigTuple(datasetPackages.mnistTransformedLimitedRotationPackage, configs.conv_64x64_6_16_ENC_1024x3_DEC_1024x3_LAT_32_bce, 500, 100, SquaredError(1), BinaryCrossEntropy()),
 
     # Shapes
-    ExperimentalConfigTuple(datasetPackages.shapesTransformedLimitedRotationPackage, configs.deepDense_64x64_ENC_1024x4_DEC_1024x4_LAT_32_bce, 500, 100, SquaredError(1), SquaredError(1.0)),
-    ExperimentalConfigTuple(datasetPackages.shapesTransformedLimitedRotationPackage, configs.conv_64x64_6_16_ENC_1024x3_DEC_1024x3_LAT_32_bce, 500, 100, SquaredError(1), SquaredError(1.0)),
+    ExperimentalConfigTuple(datasetPackages.shapesTransformedLimitedRotationPackage, configs.deepDense_64x64_ENC_1024x4_DEC_1024x4_LAT_32_bce, 500, 100, SquaredError(1), BinaryCrossEntropy()),
+    ExperimentalConfigTuple(datasetPackages.shapesTransformedLimitedRotationPackage, configs.conv_64x64_6_16_ENC_1024x3_DEC_1024x3_LAT_32_bce, 500, 100, SquaredError(1), BinaryCrossEntropy()),
 
     # NORB
-    ExperimentalConfigTuple(datasetPackages.norbPackage, configs.deepDense_96x96_ENC_1024_2048_2048_DEC_2048_2048_1024_LAT_32_bce, 500, 100, SquaredError(1), SquaredError(1.0)),
-    ExperimentalConfigTuple(datasetPackages.norbPackage, configs.conv_96x96_6_16_ENC_1024x3_DEC_1024x3_LAT_32_bce, 250, 100, SquaredError(1), SquaredError(1.0))
+    ExperimentalConfigTuple(datasetPackages.norbPackage, configs.deepDense_96x96_ENC_1024_2048_2048_DEC_2048_2048_1024_LAT_32_bce, 500, 100, SquaredError(1), BinaryCrossEntropy()),
+    ExperimentalConfigTuple(datasetPackages.norbPackage, configs.conv_96x96_6_16_ENC_1024x3_DEC_1024x3_LAT_32_bce, 250, 100, SquaredError(1), BinaryCrossEntropy())
 ]
